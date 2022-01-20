681217da9d65b59fb0782346624b9f2c9c118c96
vtrngnghia <vtrngnghia@gmail.com>
2022-01-20 08:30:20 +0700

    build: downgrade to react-router-dom@5.3.0
    react-router-dom v6 still is relatively unstable, lacks important features, and not compatible with
    react-hook-guard

--- src/features/auth/useAuthenticationGuard.ts
+++ src/features/auth/useAuthenticationGuard.ts
@@ -1,12 +1,12 @@
 import { useSelector } from "react-redux";
 import { selectAccessToken } from "./selectors";
-import { useNavigate } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 export default function useAuthenticationGuard() {
   const accessToken = useSelector(selectAccessToken);
-  const navigate = useNavigate();
+  const history = useHistory();
   if (!accessToken) {
-    navigate("/auth/login");
+    history.push("/auth/login");
   }
   return accessToken;
 }
